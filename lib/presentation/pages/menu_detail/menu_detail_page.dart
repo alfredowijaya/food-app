1e3fcf7b8ee9d98dfc797cb9b02d718bdd303ab7
Alfredo Wijaya <alfredowijaya03@gmail.com>
2023-06-03 23:04:50 +0700

    UI detail Page

--- lib/presentation/pages/menu_detail/menu_detail_page.dart
+++ lib/presentation/pages/menu_detail/menu_detail_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:food_app/constant/colors_constant.dart';
 import 'package:food_app/domain/entities/menu_detail.dart';
 import 'package:food_app/presentation/arguments/menu_detail_page_argument.dart';
 import 'package:food_app/presentation/cubits/menu_detail/menu_detail_cubit.dart';
@@ -49,83 +50,137 @@ class _MenuDetailPageState extends State<MenuDetailPage> {
               children: [
                 SingleChildScrollView(
                   child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: detail == null
-                        ? [const MenuDetailSkeleton()]
-                        : [
-                            Image.network(detail.thumbnail),
-                            Padding(
-                              padding: const EdgeInsets.all(16),
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        height: 120,
+                        width: MediaQuery.of(context).size.width,
+                        decoration: const BoxDecoration(
+                          color: ColorConstants.blackBackgound,
+                        ),
+                        padding: const EdgeInsets.symmetric(horizontal: 16),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              "TheKitchen~",
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .headlineSmall
+                                  ?.copyWith(color: ColorConstants.white),
+                            ),
+                            Container(
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 8, vertical: 4),
+                              decoration: BoxDecoration(
+                                  color: ColorConstants.primary,
+                                  borderRadius: BorderRadius.circular(8)),
+                              child: Row(
                                 children: [
+                                  const Icon(
+                                    Icons.shopping_cart,
+                                    size: 24,
+                                  ),
+                                  const SizedBox(width: 4),
                                   Text(
-                                    detail.name,
-                                    style:
-                                        Theme.of(context).textTheme.titleLarge,
-                                  ),
-                                  Text(
-                                    "${detail.category} | ${detail.area}",
-                                    style:
-                                        Theme.of(context).textTheme.bodyMedium,
-                                  ),
-                                  const SizedBox(
-                                    height: 8,
-                                  ),
-                                  Text(
-                                    "Indgridients",
-                                    style:
-                                        Theme.of(context).textTheme.titleMedium,
-                                  ),
-                                  const SizedBox(
-                                    height: 8,
-                                  ),
-                                  ...detail.ingridients.map(
-                                    (indgridient) => Text(
-                                      "${indgridient.ingredients} (${indgridient.measurement})",
-                                      style: Theme.of(context)
-                                          .textTheme
-                                          .bodyMedium,
-                                    ),
-                                  ),
-                                  const SizedBox(
-                                    height: 8,
-                                  ),
-                                  Text(
-                                    "Instruction",
-                                    style:
-                                        Theme.of(context).textTheme.titleMedium,
-                                  ),
-                                  const SizedBox(
-                                    height: 8,
-                                  ),
-                                  Text(
-                                    detail.instructions,
-                                    style:
-                                        Theme.of(context).textTheme.bodyMedium,
-                                  ),
-                                  SizedBox(
-                                    height:
-                                        MediaQuery.of(context).padding.bottom,
+                                    '2',
+                                    style: Theme.of(context)
+                                        .textTheme
+                                        .labelLarge
+                                        ?.copyWith(fontWeight: FontWeight.w600),
                                   ),
                                 ],
                               ),
-                            ),
+                            )
                           ],
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.only(
-                    top: MediaQuery.of(context).padding.top,
-                  ),
-                  padding:
-                      const EdgeInsets.only(left: 16, right: 16, bottom: 16),
-                  child: IconButton(
-                    onPressed: () => Navigator.of(context).pop(),
-                    icon: const Icon(
-                      Icons.arrow_back,
-                      color: Colors.black,
-                    ),
+                        ),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: detail == null
+                            ? [const MenuDetailSkeleton()]
+                            : [
+                                Container(
+                                  decoration: const BoxDecoration(
+                                    color: ColorConstants.blackBackgound,
+                                  ),
+                                  child: ClipRRect(
+                                    borderRadius: const BorderRadius.only(
+                                      topLeft: Radius.circular(25),
+                                      topRight: Radius.circular(25),
+                                    ),
+                                    child: Image.network(detail.thumbnail),
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.all(16),
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        detail.name,
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .headlineMedium,
+                                      ),
+                                      Text(
+                                        "${detail.category} | ${detail.area}",
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .labelSmall
+                                            ?.copyWith(
+                                              color: ColorConstants.lightGrey,
+                                            ),
+                                      ),
+                                      const SizedBox(
+                                        height: 40,
+                                      ),
+                                      Text(
+                                        "Indgridients :",
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .titleLarge,
+                                      ),
+                                      const SizedBox(
+                                        height: 8,
+                                      ),
+                                      ...detail.ingridients.map(
+                                        (indgridient) => Text(
+                                          "${indgridient.ingredients} (${indgridient.measurement})",
+                                          style: Theme.of(context)
+                                              .textTheme
+                                              .bodySmall,
+                                        ),
+                                      ),
+                                      const SizedBox(
+                                        height: 40,
+                                      ),
+                                      Text(
+                                        "Instruction :",
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .titleLarge,
+                                      ),
+                                      const SizedBox(
+                                        height: 8,
+                                      ),
+                                      Text(
+                                        detail.instructions,
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .bodySmall,
+                                      ),
+                                      SizedBox(
+                                        height: MediaQuery.of(context)
+                                            .padding
+                                            .bottom,
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                      ),
+                    ],
                   ),
                 ),
               ],
